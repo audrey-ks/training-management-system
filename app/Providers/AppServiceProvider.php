cbafb11835edd97f40151c7c6a00b8c579347b1a
audrey-ks <audreydarille@gmail.com>
2026-03-28 02:57:09 +0100

    force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,16 +1,18 @@
 <?php
-
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {}
 
     public function boot(): void
     {
-        Paginator::useBootstrapFive();
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
